4c84c361acbe60d3a63c82cd1d75a0016476c6dd
Lucas Dantas <lucas_gomesdantas@hotmail.com>
2018-06-18 15:27:29 -0300

    Implementing javadoc for radix_trie

--- radix_trie/src/com/vertumno/radix_trie/RadixTrie.java
+++ radix_trie/src/com/vertumno/radix_trie/RadixTrie.java
@@ -1,13 +1,30 @@
 package com.vertumno.radix_trie;
 
+/**
+ * @author Lucas Gomes Dantas
+ * @date June the 18th, 2018
+ * My implementation of a simple Radix Trie, developed while studying at UFRN, Brazil.
+ */
+
 import java.util.ArrayList;
 
+/**
+ * The type RadixTrie.
+ */
 public class RadixTrie
 {
-    private static Node root;
+    private Node root;
 
+    /**
+     * Instantiates a new Radix Trie with root being an empty string.
+     */
     public RadixTrie() { root = new Node(""); }
 
+    /**
+     * Receives word from the client, breaks it down to chars and inserts the new word on the Trie.
+     *
+     * @param data New word to be inserted.
+     */
     public void insert(String data)
     {
         String[] chars = data.split("");
@@ -29,6 +46,12 @@ public class RadixTrie
         }
     }
 
+    /**
+     * Search if a given word exists on the Trie.
+     *
+     * @param word Word to be searched
+     * @return True if the word exists, false otherwise.
+     */
     public boolean search(String word)
     {
         String[] chars = word.split("");
@@ -42,15 +65,62 @@ public class RadixTrie
                 count++;
             }
         }
+        /* Saying that a word exists on Trie means that all of its chars are stored and the node
+           is flagged as end of the word. */
         return (count == chars.length) && (current.endWord);
     }
 
+    /**
+     * Print all the words that are stored on this Trie.
+     */
     public void print()
     {
         ArrayList<String> words = new ArrayList<>();
         for ( String e : getWords(root, words, "") ) { System.out.println(e); }
     }
 
+    /**
+     * Receives a prefix (it does not matter if the prefix is a full word) and searchs for all words
+     * derived from this prefix.
+     *
+     * @param prefix The prefix which all words must contain
+     * @return wordsFromPrefix An ArrayList with all the words derived from the prefix
+     */
+    public ArrayList<String> getWordsFromPrefix(String prefix)
+    {
+        Node current = searchNodeByPrefix(prefix);
+        if (current != null)
+        {
+            ArrayList<String> wordsFromPrefix = new ArrayList<>();
+
+            // Gets all the words that begin with the given prefix
+            wordsFromPrefix = getWords(current, wordsFromPrefix, "");
+
+            // The words will come without the prefix, so in this loop we add the prefix to them
+            for (int i = 0; i < wordsFromPrefix.size(); i++)
+            {
+                String word = prefix + wordsFromPrefix.get(i);
+                wordsFromPrefix.set(i, word);
+            }
+
+            // And now we add the prefix itself to the list
+            wordsFromPrefix.add(prefix);
+
+            return wordsFromPrefix;
+        }
+        return null;
+    }
+
+    /**
+     * Delete a given word from the Trie, verifying all four cases of deletion.
+     * Case 1 - The word is not on the Trie;
+     * Case 2 - The node where the word ends is a leaf;
+     * Case 3 - The node where the word ends is not a leaf;
+     * Case 4 - The node's parents have a fork
+     *
+     * @param word The word to be deleted from the Trie
+     * @return True if the word was deleted, false otherwise
+     */
     public boolean delete(String word)
     {
         Node current = searchNodeEnd(word);
@@ -123,15 +193,5 @@ public class RadixTrie
             if ( child.hasChildren() ) { getWords(child, words, data + child.data); }
         }
         return words;
-    }
-
-    public void getWordsFromPrefix(String prefix)
-    {
-        Node current = searchNodeByPrefix(prefix);
-        if (current != null)
-        {
-            ArrayList<String> wordsFromPrefix = new ArrayList<>();
-            wordsFromPrefix = getWords(current, wordsFromPrefix, "");
-        }
     }
 }
--- radix_trie/src/com/vertumno/usage/Main.java
+++ radix_trie/src/com/vertumno/usage/Main.java
@@ -1,14 +1,28 @@
 package com.vertumno.usage;
 
+/**
+ * @author Lucas Gomes Dantas
+ * @date June the 18th, 2018
+ * Main class to only call a few functions and check how it works.
+ */
+
 import com.vertumno.radix_trie.RadixTrie;
 
+/**
+ * The type Main.
+ */
 public class Main
 {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args)
     {
         RadixTrie rt = new RadixTrie();
         String[] toInsert = {"the", "a", "there", "answer", "any", "by", "bye", "their"};
         for(String e : toInsert) rt.insert(e);
-        rt.print();
+        for(String j : rt.getWordsFromPrefix("the")) System.out.println(j);
     }
 }
